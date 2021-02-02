120c18aa91e5139392d1ebac2f88717839966702
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-02-02 12:56:56 -0300

    Added an operate function that takes two numbers and an operator and calls the corresponding math operator function, giving a result of the operation.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-//CALCULATOR FUNCTIONS 
+
+
+//FUNCTIONS
+//Basic Math operators 
 function add (a, b) {
 	return a + b;
 }
@@ -13,4 +16,23 @@ function multiply (a, b) {
 
 function divide(a, b) {
 	return a / b;	
+}
+
+//Operation
+function operate(a, b, operator) {
+    switch (operator) {
+        case "sum":
+            result = add(a, b);
+            break;
+        case "subtraction":
+            result = subtract(a, b);
+            break;
+        case "multiplication":
+            result = multiply(a, b);
+            break;
+        case "division":
+            result = divide(a, b);
+            break;
+    }
+    return result;
 }
